6a092f396fe6c90d0d03b152785067cdb2d6bee2
z5363406 <z5363406@vx12.orchestra.cse.unsw.EDU.AU>
2023-01-11 22:47:12 +1100

    added error handling to completed commands

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 import data
 from table2ascii import table2ascii as t2a, PresetStyle
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -21,8 +22,12 @@ async def on_ready():
 @bot.command(name='curr_price', help='Get the current price of one or more stocks')
 async def curr_price(ctx, *args):
     ret_array = []
+    non_existent = []
     for tag in args:
-        ret_array.append([str(tag), f"${round(data.current_price(str(tag)), 2)}"])
+        if data.ticker_exists(str(tag)):
+            ret_array.append([str(tag), f"${round(data.current_price(str(tag)), 2)}"])
+        else:
+            non_existent.append(str(tag))
 
     output = t2a(
         header=["Ticker", "Price"],
@@ -32,16 +37,39 @@ async def curr_price(ctx, *args):
 
     await ctx.send(f"```\n{output}\n```")
     
+    if len(non_existent) > 0:
+        await ctx.send(f"{', '.join(non_existent)} symbol/s do not exist")
+
+    
     
 @bot.command(name='info', help='Get info of a particular stock according to the list of keys')
 async def get_info(ctx, symbol: str, key: str):
-    await ctx.reply(data.get_info(symbol, key))
+    if not data.ticker_exists(symbol):
+        await ctx.send(f"Ticker symbol {symbol} does not exist or may be delisted.")
+    else:
+        try:
+            await ctx.send(data.get_info(symbol, key))
+        except KeyError:
+            await ctx.send(f"{key} is not a valid information identifier")
+            
+@get_info.error
+async def flip_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Incorrect arguments entered. Please enter: !get_info \{ticker symbol\} \{information requested\}")
+        
+        
+@bot.command(name='balance_sheet', help='Returns the most recent balance sheet of a single company specified by the ticker symbol entered')
+async def balance_sheet(ctx, symbol: str):
+    pass
 
 bot.run(TOKEN)
 
-# command to get % change in a certain sector ? 
+
 # command for % change in stock price over a specified time period
 # command to get a graph of a stock
 # command to get prices of stocks
 # command for balance sheet
+# command for earnings
+# maybe an earnings graph for each quarter? 
+# command for info
 
--- data.py
+++ data.py
@@ -1,13 +1,16 @@
 import yfinance as yf
 import pandas as pd
 
-
+def ticker_exists(symbol):
+    data = yf.Ticker(symbol).history(period='1d', interval='1m')
+    data_list = data.to_numpy().tolist()
+    if len(data_list) == 0:
+        return False
+    return True
 
 def current_price(symbol):
     data = yf.Ticker(symbol).history(period='1d', interval='1m')
     data_list = data.to_numpy().tolist()
-    if len(data_list) == 0:
-        return None
     return data_list[-1][3]
 
 def get_balance_sheet(symbol):
@@ -24,5 +27,3 @@ def get_info(symbol, info_required):
 # how should we get the % change (maybe % change over a time interval like 5y, 1y, 6 months and 1 month)
 def get_percentage_change(symbol):
     pass
-
-print(get_info('BDBDBD', 'sector'))
